017e877682f63f0f7a78ad958abb2adbfb6ab8c4
gjwilliams716 <gjwilliams716@gmail.com>
2023-10-03 16:59:01 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,7 +1,7 @@
 <?php
 function get_db_connection(){
     // Create connection
-    $conn = new mysqli(159.89.47.44, 'willmiso_hw3user', '!P4n-w_Fhdpy', 'willmiso_hw3');
+    $conn = new mysqli('159.89.47.44', 'willmiso_hw3user', '!P4n-w_Fhdpy', 'willmiso_hw3');
     
     // Check connection
     if ($conn->connect_error) {
