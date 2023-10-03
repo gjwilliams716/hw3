fee688c5f0ad3bfe6917d3d9d772e5992901bde0
gjwilliams716 <gjwilliams716@gmail.com>
2023-10-03 16:01:49 -0500

    Create util-db.php

---
+++ util-db.php
@@ -0,0 +1,12 @@
+<?php
+function get_db_connection(){
+    // Create connection
+    $conn = new mysqli(159.89.47.44, 'willmiso_hw3user', '!P4n-w_Fhdpy', 'willmiso_hw3');
+    
+    // Check connection
+    if ($conn->connect_error) {
+      return false;
+    }
+    return $conn;
+}
+?>
