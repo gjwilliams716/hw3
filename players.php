1739a942c426fbc2dde4ca784c0f2e1aae753c36
gjwilliams716 <gjwilliams716@gmail.com>
2023-10-03 16:08:08 -0500

    Create players.php

---
+++ players.php
@@ -0,0 +1,13 @@
+<?php
+  require_once("util-db.php");
+  require_once("model-players.php")
+  
+  $pageTitle= "Players";
+
+  include "view-header.php";
+
+  $players = selectPlayers();
+  include "view-players.php";
+
+  include "view-footer.php";
+?>
