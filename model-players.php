f2751f0a8dad6151b678f119b60d617e98b8f4fb
gjwilliams716 <gjwilliams716@gmail.com>
2023-10-03 16:13:20 -0500

    Create model-players.php

---
+++ model-players.php
@@ -0,0 +1,15 @@
+<?php
+function selectPlayers() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT player_id, player_name, player_team FROM `player`;");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
