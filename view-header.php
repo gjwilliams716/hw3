80e558878d7ad7f5c18bda37201ea114b9d96e92
gjwilliams716 <gjwilliams716@gmail.com>
2023-10-03 15:49:10 -0500

    Create view-header.php

---
+++ view-header.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title> <?=$pageTitle?> </title>
+  </head>
+  <body>
+    <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+<nav class="nav nav-pills nav-justified">
+  <a class="nav-item nav-link active" href="/">Home</a>
+  <a class="nav-item nav-link" href="post.php">Post</a>
+  <a class="nav-item nav-link" href="post-result.php">Post Result</a>
+  <a class="nav-item nav-link disabled" href="get.php">Get</a>
+  <a class="nav-item nav-link disabled" href="get-result.php">Get Result</a>
+</nav>
+</nav>
