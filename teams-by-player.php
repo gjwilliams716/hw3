eb8daac8ea9c08d9fce33fcac732e286ec3cf2f0
gjwilliams716 <gjwilliams716@gmail.com>
2023-10-03 20:18:58 -0500

    Update teams-by-player.php

--- teams-by-player.php
+++ teams-by-player.php
@@ -1,1 +1,10 @@
-
+<?php
+  require_once("util-db.php");
+  require_once("model-teams-by-player.php");
+  
+  $pageTitle= "Teams by Player";
+  include "view-header.php";
+  $teams = selectTeamsByInstructor($_GET['id']);
+  include "view-courses-by-instructor.php";
+  include "view-footer.php";
+?>
