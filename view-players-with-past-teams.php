124ee87bfc4ea848494b9e784f56f2c01904e607
gjwilliams716 <gjwilliams716@gmail.com>
2023-10-03 22:17:20 -0500

    Update view-players-with-past-teams.php

--- view-players-with-past-teams.php
+++ view-players-with-past-teams.php
@@ -8,14 +8,22 @@
           <div class="card-body">
           <h5 class="card-title"><?php echo $player['player_name'] ?></h5>
           <p class="card-text">
+                <ul class="list-group">
             <?php
               $teams = selectTeamsByPlayer($player['player_id']);
                 while ($team = $teams->fetch_assoc()){
                     ?>
-                      
+                      <li class="list-group-item"><?php echo $contract['team_name']; ?> - <?php echo $contract['team_owner']; ?> - <?php echo $contract['contract_length']; ?> - <?php echo $contract['contract_amount']; ?> - <?php echo $contract['contract_incentives']; ?></li>
+                    <td><?php echo $contract['team_id']; ?></td>
+                    <td><?php echo $contract['team_name']; ?></td>
+                    <td><?php echo $contract['team_owner']; ?></td>
+                    <td><?php echo $contract['contract_length']; ?></td>
+                    <td><?php echo $contract['contract_amount']; ?></td>
+                    <td><?php echo $contract['contract_incentives']; ?></td>
                   <?php    
                 }
             ?>
+                </ul>
           </p>
           <p class="card-text"><small class="text-muted">Team(s): <?php echo $player['player_team'] ?></small></p>
     </div>
