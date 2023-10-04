622ab344d1349d38175dad717b264cd46c04de34
gjwilliams716 <gjwilliams716@gmail.com>
2023-10-03 21:10:46 -0500

    Update model-teams-by-player.php

--- model-teams-by-player.php
+++ model-teams-by-player.php
@@ -3,7 +3,7 @@ function selectTeamsByPlayer($pid) {
 try {
 $conn = get_db_connection();
 $stmt = $conn->prepare("SELECT t.team_id, team_name, team_owner, contract_length, contract_amount, contract_incentives FROM `team` t join contract c on t.team_id = c.team_id where c.player_id = ?");
-$stmt->bind_param("s", $pid);
+$stmt->bind_param("p", $pid);
 $stmt->execute();
 $result = $stmt->get_result();
 $conn->close();
