26bcdbdf7c64a29d6c3b3ec0da9cc7c24a850e03
gjwilliams716 <gjwilliams716@gmail.com>
2023-10-03 22:29:46 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,8 @@ $pageTitle= "Home";
 include "view-header.php";
 ?>
     <h1>Homework 3</h1>
+<p class="text-left">This is my homework that uses my database about baseball players in a fictional baseball league.</p>
+<p class="text-left">I'll add more players to this database as the league grows.</p>
     <?php
 include "view-footer.php";
 ?>
