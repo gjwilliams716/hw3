3022acdbb685c325a1c797f5254bc07a40543a29
gjwilliams716 <gjwilliams716@gmail.com>
2023-10-24 17:20:55 -0500

    Update teams.php

--- teams.php
+++ teams.php
@@ -8,7 +8,11 @@
   if (isset($_POST['actionType'])){
       switch ($_POST['actionType']) {
         case "Add":
-        insertTeam($_POST['tName'], $_POST['tOwner']);
+        if (insertTeam($_POST['tName'], $_POST['tOwner'])){
+        echo '<div class="alert alert-success" role="alert">Team Added.</div>"';
+        }else{
+        echo '<div class="alert alert-danger" role="alert">Error.</div>"';
+        }
         break;
       }
   }
