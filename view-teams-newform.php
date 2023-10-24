954f093c08d7754a5186c02c36c39412dab93c36
gjwilliams716 <gjwilliams716@gmail.com>
2023-10-24 16:21:23 -0500

    Update view-teams-newform.php

--- view-teams-newform.php
+++ view-teams-newform.php
@@ -15,7 +15,7 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-          <form method="post" action="view-teams-newform.php">
+          <form method="post" action="">
                 <div class="mb-3">
                   <label for="tName" class="form-label">Team Name</label>
                   <input type="text" class="form-control" id="tName" name="tName">
